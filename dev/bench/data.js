36bd22f8440b40101b5c8190654f1aba1202c71c
github-action-benchmark <github@users.noreply.github.com>
2025-03-20 12:41:37 +0000

    add Test second action benchmark result for 051665a26686db61349d3bcdf6e21b08a22e5999

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1742474497614,
+  "repoUrl": "https://github.com/cryspen/benchmark-upload-and-plot-action",
+  "entries": {
+    "Test second action": [
+      {
+        "commit": {
+          "author": {
+            "name": "cryspen",
+            "username": "cryspen"
+          },
+          "committer": {
+            "name": "cryspen",
+            "username": "cryspen"
+          },
+          "id": "051665a26686db61349d3bcdf6e21b08a22e5999",
+          "message": "Add a workflow to test both actions at once",
+          "timestamp": "2025-03-20T11:12:50Z",
+          "url": "https://github.com/cryspen/benchmark-upload-and-plot-action/pull/2/commits/051665a26686db61349d3bcdf6e21b08a22e5999"
+        },
+        "date": 1742474497343,
+        "bigger_is_better": false,
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 135,
+            "unit": "ns/iter",
+            "range": "± 24"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 18149,
+            "unit": "ns/iter",
+            "range": "± 755"
+          }
+        ]
+      }
+    ]
+  }
+}
