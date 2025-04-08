828e0913e174f77366ff5f41d8743ed59eb83d3b
wysiwys <clara@cryspen.com>
2025-04-08 11:35:22 +0200

    use relative path

--- src/default_index_html.ts
+++ src/default_index_html.ts
@@ -1,2 +1,2 @@
 import * as fs from 'fs';
-export const DEFAULT_INDEX_HTML = fs.readFileSync('default_index.html', 'utf-8');
+export const DEFAULT_INDEX_HTML = fs.readFileSync('./default_index.html', 'utf-8');
