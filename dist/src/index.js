f4b44b9345209ce799df24b82af11615d9288cc4
wysiwys <clara@cryspen.com>
2025-04-22 09:18:09 +0200

    add stack trace

--- dist/src/index.js
+++ dist/src/index.js
@@ -31,5 +31,14 @@ async function main() {
     await (0, write_1.writeBenchmark)(bench, config);
     console.log('github-action-benchmark was run successfully!', '\nData:', bench);
 }
-main().catch((e) => core.setFailed(e.message));
+main().catch((e) => {
+    function stackTrace(e) {
+        const prefix = 'Error: ' + e.message;
+        // get the stack trace without the error message
+        const stackTrace = String(e.stack).replace(prefix, 'Stack trace:');
+        return stackTrace;
+    }
+    console.log(stackTrace(e));
+    core.setFailed(e.message);
+});
 //# sourceMappingURL=index.js.map
